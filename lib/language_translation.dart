d9a0f3fb86f883510836846c3268e22b0ebee5b5
bhaskarbiswas04 <bhaskarb45nov@gmail.com>
2024-07-03 21:21:15 +0530

    first

---
+++ lib/language_translation.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class LanguageTranslation extends StatefulWidget {
+  const LanguageTranslation({super.key});
+
+  @override
+  State<LanguageTranslation> createState() => _LanguageTranslationState();
+}
+
+class _LanguageTranslationState extends State<LanguageTranslation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Language Translator'),
+        titleTextStyle: const TextStyle(
+          fontSize: 25,
+          color: Colors.black,
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.teal,
+        elevation: 0,
+      ),
+    );
+  }
+}
